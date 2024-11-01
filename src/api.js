57ab222d3270bf2cd634ce007d329f5ec9ea5e48
Kasun3865 <ict21945@fot.sjp.ac.lk>
2024-11-01 15:43:15 +0530

    Api Added

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const searchImages = async () => {
+const searchImages = async (terms) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
             Authorization: 'Client-ID pMebQ8NuC4L1yDwOkH8BJ2ALG1MEnHL0IYcTgJFSMo4'
         },
         params: {
-            query: 'cars'
+            query: 'terms'
         }
     })
     console.log(response);
